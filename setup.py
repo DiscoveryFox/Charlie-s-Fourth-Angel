e81f88a319846945cd22ef0acc0d1cb0dc1e6be1
discovery-bit <61625792+discovery-bit@users.noreply.github.com>
2022-03-02 23:44:53 +0100

    Some Project Files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Charlie-s-Fourth-Angel',
+    version='1.0',
+    packages=[],
+    url='flinn.eu/Charlie-s-Fourth-Angel',
+    license='GNU General Public License v3.0',
+    author='Discovery Fox',
+    author_email='Flinn@Fuxxbau.de',
+    description='A website that allows the user to use many hacking scripts, normally only executable in the terminal, based on a simple GUI.'
+)
