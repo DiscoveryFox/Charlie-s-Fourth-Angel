85a00d92f5a369a683f78fa70a1f0c3161f2da5a
discovery-bit <61625792+discovery-bit@users.noreply.github.com>
2022-03-24 21:52:59 +0100

    Small Changes

--- tool_installer.py
+++ tool_installer.py
@@ -1,5 +1,12 @@
 import os
+import pip
 
 
-def install(gitlink: str) -> None:
-    os.system(f'git clone {gitlink}')
+def install(pipname=None, gitlink=None):
+    if pipname is None:
+        os.system(f'git clone {gitlink}')
+    else:
+        if hasattr(pip, 'main'):
+            pip.main(['install', pipname])
+        else:
+            pip._internal.main(['install', pipname])
