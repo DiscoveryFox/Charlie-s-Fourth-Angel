0299ad6be9518ea4367bdd25824d5431a4aab0b9
discovery-bit <61625792+discovery-bit@users.noreply.github.com>
2022-04-22 17:14:11 +0200

    changes how the nmap backend works

--- service_nmap.py
+++ service_nmap.py
@@ -7,5 +7,24 @@ nm = nmap.PortScanner()
 
 # todo: set the path to the nmap directory
 
-def usenmap(ip: str, port: str):
-    return nm.scan(ip, port)
+def usenmap(ip: str, port: str, json_output: bool = False):
+    """
+    :param ip: ip address
+    :param port: port number
+    :param json_output: json output
+    :return feedback: dictionary or html like file
+    """
+    if json_output is True:
+        return nm.scan(ip, port)
+
+    result = nm.scan(ip, port)
+
+    # render a html like file which contains the result prettified
+
+    for host in result['scan']:
+        for port in result['scan'][host]['tcp']:
+            if result['scan'][host]['tcp'][port]['state'] == 'open':
+                feedback.append(
+                    f'{host}:{port}/{result["scan"][host]["tcp"][port]["name"]}')
+
+    return feedback
