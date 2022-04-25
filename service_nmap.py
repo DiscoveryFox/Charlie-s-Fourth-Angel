b4563e5209a47fde16081ac1fea2cb6d063c94db
discovery-bit <61625792+discovery-bit@users.noreply.github.com>
2022-04-25 22:30:32 +0200

    started to develop a ipv6 support for nmap

--- service_nmap.py
+++ service_nmap.py
@@ -37,6 +37,12 @@ def usenmap(ip: str, port: typing.Union[str, list] = None, json_output: bool = F
     """
     global right_ip
 
+    ipv4_regex = re.compile(
+        r"^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$")
+
+    ipv6_regex = re.compile(
+        r"(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))")
+
     # check if only one port is given or if the port is a range
     if port is list:
         single_port = False
@@ -49,7 +55,10 @@ def usenmap(ip: str, port: typing.Union[str, list] = None, json_output: bool = F
         else:
             return nm.scan(ip)
 
-    result = nm.scan(ip, port)
+    if ipv6_regex.match(ip):
+        result = nm.scan(ip, port, arguments='-6')
+    else:
+        result = nm.scan(ip, port)
 
     # if ip is not a valid ip address print("Invalid IP address")
     # use regex to check if ip is valid.
@@ -58,18 +67,13 @@ def usenmap(ip: str, port: typing.Union[str, list] = None, json_output: bool = F
     # xxx is a number between 0 and 255
     # xxx.xxx.xxx.xxx is a valid ip address
     # if ip is not valid print("Invalid IP address")
-
-    ipv4_regex = re.compile(
-        r"^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$")
+    pprint(result)
 
     # create a regex pattern for an ipv6 address
     # and ipv6 address looks like this:
     # xxxx:xxxx:xxxx:xxxx:xxxx:xxxx:xxxx:xxxx
     # xxxx is a number between 0 and 65535 it can contain chars
     # xxxx:xxxx:xxxx:xxxx:xxxx:xxxx:xxxx:xxxx is a valid ipv6 address
-
-    ipv6_regex = re.compile(
-        r"(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))")
 
     if not ipv4_regex.match(ip) and not ipv6_regex.match(ip):
         ip = get_ip(ip)
@@ -79,10 +83,17 @@ def usenmap(ip: str, port: typing.Union[str, list] = None, json_output: bool = F
     doc = dominate.tags.div(cls='popup')
     doc.add(dominate.tags.a('×', href='#', cls='close'))
 
+    if result['nmap']['scanstats']['downhosts'] == '1':
+        doc.add(dominate.tags.h2(f'Nmap scan results for {ip}'))
+        doc.add(dominate.tags.p(f'Host is down no scan available.'))
+        gdoc.add(doc)
+        return {"content": gdoc.render()}
+
     doc.add(dominate.tags.h2(f'Nmap scan results for '
                              f'{result["scan"][ip]["addresses"]["ipv4"]}'))
     try:
         doc.add(dominate.tags.p(f'Hostname: {result["scan"][ip]["hostnames"][0]["name"]}'))
+        doc.add(dominate.tags.p(f'Type: {result["scan"][ip]["hostnames"][0]["type"]}'))
         # doc.add(dominate.tags.p(f'Hostname of target: {socket.gethostbyaddr(ip)[0]} '))
     except Exception as e:
         print(e.__traceback__.tb_frame.f_globals["__file__"])
@@ -91,7 +102,19 @@ def usenmap(ip: str, port: typing.Union[str, list] = None, json_output: bool = F
     doc.add(dominate.tags.p(f'Time to scan: {result["nmap"]["scanstats"]["elapsed"]}s'))
     doc.add(dominate.tags.p(f'Date of scan: {result["nmap"]["scanstats"]["timestr"]}'))
     if single_port is True:
+        doc.add(dominate.tags.p(f'Server: {result["scan"][ip]["status"]["state"]}'))
+        doc.add(dominate.tags.p(f'Method to check Server: {result["scan"][ip]["status"]["reason"]}'))
         doc.add(dominate.tags.p(f'Port {port} is {result["scan"][ip]["tcp"][int(port)]["state"]}'))
+        doc.add(dominate.tags.p(f'Reason: {result["scan"][ip]["tcp"][int(port)]["reason"]}'))
+        doc.add(dominate.tags.p(f'Service: {result["scan"][ip]["tcp"][int(port)]["name"]}'))
+        if result["scan"][ip]["tcp"][int(port)]["product"]:
+            doc.add(dominate.tags.p(f'Product: {result["scan"][ip]["tcp"][int(port)]["product"]}'))
+        if result["scan"][ip]["tcp"][int(port)]["version"]:
+            doc.add(dominate.tags.p(f'Version: {result["scan"][ip]["tcp"][int(port)]["version"]}'))
+        if result["scan"][ip]["tcp"][int(port)]["extrainfo"]:
+            doc.add(dominate.tags.p(f'Extra Info: {result["scan"][ip]["tcp"][int(port)]["extrainfo"]}'))
+        if result["scan"][ip]["tcp"][int(port)]["cpe"]:
+            doc.add(dominate.tags.p(f'CPE: {result["scan"][ip]["tcp"][int(port)]["cpe"]}'))
     else:
         for singled_port in port:
             doc.add(dominate.tags.p(
