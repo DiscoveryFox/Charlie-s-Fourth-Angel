adac7c4163bc49980e470d16377ed7d52f211122
discovery-bit <61625792+discovery-bit@users.noreply.github.com>
2022-04-30 00:44:44 +0200

    created small server which returns all tools + code for them later. need to move out to external ip hosted by me. or make a version which can be hosted by everyone.

---
+++ tests/serviceworker.py
@@ -0,0 +1,20 @@
+import flask
+import configparser
+import json
+
+config = configparser.ConfigParser()
+config.read('/home/kali/Documents/Charlie-s-Fourth-Angel/app.cfg')
+
+app = flask.Flask(__name__)
+
+
+@app.route('/')
+def index():
+    # services_path = config['PATHS']['ServicesPath']
+    # print(services_path)
+    with open('Services.json', 'r') as f:
+        return json.loads(f.read())
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5001)
